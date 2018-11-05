6e0b442402d3f90d8444ae7839fb98fe894c0907
camille <mouttecam@gmail.com>
2018-11-05 12:47:28 +0100

    MatriceAdjacenteClasse : edit private attribut

--- Documents/workspace/ProjetGraphe/src/graphe/MatriceAdjacenteClasse.java
+++ Documents/workspace/ProjetGraphe/src/graphe/MatriceAdjacenteClasse.java
@@ -1,52 +1,53 @@
 package graphe;
 
 public class MatriceAdjacenteClasse {
-	
 	//matrice d'adjacence
-	private String[][] matriceAdjacenteTab;
+	// modifier avant c'était en privé
+		public String[][] matriceAdjacenteTab;
 
-	
-	int sommetInit;
-	int sommetTerm;
-	int valeur;
+		
+		int sommetInit;
+		int sommetTerm;
+		int valeur;
 
-	public MatriceAdjacenteClasse(String[][] matAds)
-	{
-		matriceAdjacenteTab = matAds;
-	}
-	public void afficheMatAdj()
-	{
-		System.out.println("Matrice d'adjacence");
-		System.out.print("  ");
-		for (int j = 0; j < matriceAdjacenteTab.length; j++)
+		public MatriceAdjacenteClasse(String[][] matAds)
 		{
-			System.out.print(j + " ");
+			matriceAdjacenteTab = matAds;
 		}
-		System.out.println("");
-		for (int i = 0; i < matriceAdjacenteTab.length; i++)
+		public void afficheMatAdj()
 		{
-			System.out.print(i + " ");
+			System.out.println("Matrice d'adjacence");
+			System.out.print("  ");
 			for (int j = 0; j < matriceAdjacenteTab.length; j++)
 			{
-				System.out.print(matriceAdjacenteTab[i][j] + " ");
+				System.out.print(j + " ");
 			}
 			System.out.println("");
-		}
-	}
-	
-	public void initMatAdj(String[][] matriceAdjacente_t)
-	{
-		for (int i = 0; i < matriceAdjacente_t.length; i++)
-		{
-			for (int j = 0; j < matriceAdjacente_t.length; j++)
+			for (int i = 0; i < matriceAdjacenteTab.length; i++)
 			{
-				matriceAdjacente_t[i][j] = "F";
+				System.out.print(i + " ");
+				for (int j = 0; j < matriceAdjacenteTab.length; j++)
+				{
+					System.out.print(matriceAdjacenteTab[i][j] + " ");
+				}
+				System.out.println("");
 			}
 		}
-	}
-	
-	public void remplirMatAdj(String[][] matriceAdjacence, int sommetInit, int sommetTerm,int valeur)
-	{
-		matriceAdjacenteTab[sommetInit][sommetTerm] = "V";
-	}
+		
+		public void initMatAdj(String[][] matriceAdjacente_t)
+		{
+			for (int i = 0; i < matriceAdjacente_t.length; i++)
+			{
+				for (int j = 0; j < matriceAdjacente_t.length; j++)
+				{
+					matriceAdjacente_t[i][j] = "F";
+				}
+			}
+		}
+		
+		public void remplirMatAdj(String[][] matriceAdjacence, int sommetInit, int sommetTerm,int valeur)
+		{
+			matriceAdjacenteTab[sommetInit][sommetTerm] = "V";
+		}
+
 }
